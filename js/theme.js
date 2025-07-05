48e3d27db930cbea3dadd43828499a43ef65f040
mansi ruhil <157950124+mansiruhil@users.noreply.github.com>
2025-07-05 12:55:51 +0530

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,1 +1,9 @@
+(function ($, Drupal) {
+  "use strict";
+  Drupal.behaviors.oliveroSubTheme = {
+    attach: function (context, settings) {
+      // My custom js.
+    },
+  };
+})(jQuery, Drupal);
 
